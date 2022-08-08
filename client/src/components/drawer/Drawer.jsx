607fd9c4d3256c8784152c613b60fffa3138d384
Laboimagen <usuario.laboimagen@gmail.com>
2022-08-08 16:24:05 -0500

    cambios hot

--- client/src/components/drawer/Drawer.jsx
+++ client/src/components/drawer/Drawer.jsx
@@ -154,7 +154,7 @@ function ResponsiveDrawer(props) {
           </IconButton>
           <div>
           <Typography variant="h6" noWrap component="div" color="primary">
-            Clinic Soft
+            LaboImagen
           </Typography>
           <Typography variant="body2" color="initial" sx={{color:'gray'}}>Sistema de laboratorio clínico</Typography>
           </div>
